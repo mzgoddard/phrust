9d7a49ed774fb5b229148296aa59735ce9b38879
Michael "Z" Goddard <mzgoddard@gmail.com>
2015-01-26 00:30:34 -0500

    Add initial Particle struct.

---
+++ particle.rs
@@ -0,0 +1,53 @@
+use std::default::Default;
+pub use super::math::*;
+
+#[derive(Clone, Copy, Show)]
+pub enum State {
+  Dynamic,
+  Asleep,
+  Trigger,
+  Static,
+}
+
+#[derive(Clone, Copy, Show)]
+pub struct Particle {
+  pub id: u32,
+  pub position: V2,
+  pub last_position: V2,
+  pub acceleration: V2,
+  pub radius: f64,
+  pub radius2: f64,
+  pub mass: f64,
+  pub friction: f64,
+  pub factor: f64,
+  pub state: State,
+  pub old_ddvt_position: V2,
+}
+
+impl Particle {
+  pub fn at(p: V2) -> Particle {
+    Particle {
+      position: p,
+      last_position: p,
+      .. Default::default()
+    }
+  }
+}
+
+impl Default for Particle {
+  fn default() -> Particle {
+    Particle {
+      id: Default::default(),
+      position: Default::default(),
+      last_position: Default::default(),
+      acceleration: Default::default(),
+      radius: 1f64,
+      radius2: 1f64,
+      mass: 1f64,
+      friction: 0.99f64,
+      factor: 0.5f64,
+      state: State::Dynamic,
+      old_ddvt_position: Default::default(),
+    }
+  }
+}
